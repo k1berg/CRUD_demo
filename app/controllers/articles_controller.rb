33522e92363386de5902ecbcb1ffddbde23f5f9f
k1berg <tarn.aler@gmail.com>
2024-03-01 00:11:14 +0300

    added edit, update, destroy controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,34 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+      flash[:success] = 'Updated'
+    else
+      render :edit
+      flash[:failure] = 'Fail'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+
+    if @article.destroy
+      redirect_to root_path
+      flash[:success] = 'Dropped'
+    else
+      redirect_to article_path(@article)
+      flash[:failure] = 'Fail'
+    end
+  end
+
   private
 
   def article_params
